7437f99b74c2757d0e476b3f0f4265f5b24e52cd
fengqi <wangfq@zetyun.cn>
2026-08-18 13:27:38 +0800

    chore(release): prepare v2.0.0-rc.1

--- frontend/apps/site/src/App.tsx
+++ frontend/apps/site/src/App.tsx
@@ -97,7 +97,7 @@ function ServerInstallBlock({ locale }: { locale: Locale }) {
   const [copied, setCopied] = useState(false);
   const command = `helm upgrade --install kubeloop \\
   oci://ghcr.io/fengqi-dev/kube-loop/charts/kubeloop \\
-  --version 2.0.0-beta.9 \\
+  --version 2.0.0-rc.1 \\
   --namespace kubeloop-system \\
   --create-namespace \\
   --set publicURL=http://kubeloop.example.com \\
