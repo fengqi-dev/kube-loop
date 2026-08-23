654f08a6512e79ad3f3378c20c3fe75af49ee617
fengqi <fengqi0508@gmail.com>
2026-08-23 11:28:31 +0800

    refactor(control-plane): split configuration responsibilities

--- cmd/kubeloop-control-plane/main.go
+++ cmd/kubeloop-control-plane/main.go
@@ -12,7 +12,6 @@ import (
 	"syscall"
 
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 
 	internalcli "github.com/fengqi-dev/kube-loop/internal/cli"
 	"github.com/fengqi-dev/kube-loop/internal/controlplane"
@@ -87,24 +86,6 @@ func newControlPlaneCommand(stdout io.Writer) *cobra.Command {
 		}
 	}
 	return command
-}
-
-func controlPlaneOptionsFrom(config *viper.Viper) string {
-	return strings.TrimSpace(config.GetString("control-plane.config-file"))
-}
-
-func applyControlPlaneOverrides(
-	config *viper.Viper,
-	loaded loadedControlPlaneConfig,
-) (loadedControlPlaneConfig, error) {
-	config.SetDefault("control-plane.api.listen", loaded.Document.API.Listen)
-	config.SetDefault("control-plane.api.public-url", loaded.Document.API.PublicURL)
-	config.SetDefault("control-plane.logging.level", loaded.Document.Logging.Level)
-	document := loaded.Document
-	document.API.Listen = config.GetString("control-plane.api.listen")
-	document.API.PublicURL = config.GetString("control-plane.api.public-url")
-	document.Logging.Level = config.GetString("control-plane.logging.level")
-	return normalizeControlPlaneConfig(document)
 }
 
 func runControlPlane(
